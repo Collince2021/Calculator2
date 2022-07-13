c59e508a3d52652cf6c6e6992ca7fa0bc21c5a7a
Collince2021 <okotocollince@gmail.com>
2022-07-13 10:06:54 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let allButtons = Array.from(document.getElementsByClassName("button"));
+const display = document.getElementById("output");
+allButtons.map((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.innerText) {
+      case "AC":
+        display.innerText = "";
+        break;
+      case "Del":
+        display.innerText = display.innerText.slice(0, -1);
+        break;
+      case "=":
+        display.innerText = eval(display.innerText);
+
+        break;
+      default:
+        if (display.innerText === ".") return;
+        display.innerText += e.target.innerText;
+        break;
+    }
+  });
+});
+
